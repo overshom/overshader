de4896f9a6ab79868724f316e1d17889c851774a
Oleksii Shnyra <shnura94@gmail.com>
2022-05-29 19:54:54 +0300

    chore(demo): ignore changes to git for live server

--- github-demo/live-server.js
+++ github-demo/live-server.js
@@ -10,7 +10,7 @@ const params = {
   port: 3200, // Set the server port. Defaults to 8080.
   host: '0.0.0.0', // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
   root: path.resolve(process.cwd(), '../'), // Set root directory that's being served. Defaults to cwd.
-  ignore: '', // comma-separated string for paths to ignore
+  ignore: '**/.git/**', // comma-separated string for paths to ignore
   open: false,
   file: 'github-demo/index.html', // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
   wait: 0, // Waits for all changes, before reloading. Defaults to 0 sec.

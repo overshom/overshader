8af4ba72db15db77fea385e94b35af3af58e93be
Oleksii Shnyra <shnura94@gmail.com>
2022-05-29 17:05:09 +0300

    chore(demo): setup live server, add shaders gallery

--- github-demo/index.js
+++ github-demo/index.js
@@ -3,17 +3,87 @@
  */
 const { runWebglProgram } = Overshader;
 
-runWebglProgram({
-  canvasContainer: '.canvas1',
-  fragmentShaderURL: '/assets/shaders/metaball.glsl',
-  contextAttributes: {
-    alpha: false,
-    antialias: false,
-  },
-  //   textures: [
-  //     "/assets/textures/texture-font.png",
-  //     "/assets/textures/texture-noise.png",
-  //   ],
-  // lower resolution for Retina screen for better performance
-  //   devicePixelRatio: 1,
+const gallery = document.querySelector('.gallery');
+
+if (!gallery) {
+  throw new Error('gallery not found');
+}
+
+let counter = 0;
+/**
+ *
+ * @param {(selector: string) => void} cb
+ */
+const insideCanvas = cb => {
+  const className = `canvas${++counter}`;
+  const div = document.createElement('div');
+  div.onclick = () => div.requestFullscreen();
+  div.classList.add(className);
+  gallery.appendChild(div);
+  cb(`.${className}`);
+};
+
+insideCanvas(selector => {
+  runWebglProgram({
+    canvasContainer: selector,
+    fragmentShaderURL: '/assets/shaders/tubes.glsl',
+    textures: ['/assets/textures/texture-soil.jpeg'],
+    // lower resolution for Retina screen for better performance
+    devicePixelRatio: 1,
+  });
+});
+
+insideCanvas(selector => {
+  runWebglProgram({
+    canvasContainer: selector,
+    fragmentShaderURL: '/assets/shaders/glass.glsl',
+    contextAttributes: {
+      alpha: false,
+    },
+  });
+});
+
+insideCanvas(selector => {
+  runWebglProgram({
+    canvasContainer: selector,
+    fragmentShaderURL: '/assets/shaders/matrix.glsl',
+    textures: ['/assets/textures/texture-font.png', '/assets/textures/texture-noise.png'],
+    initialTimeShift: Math.random() * 1e3 * 1,
+  });
+});
+
+insideCanvas(selector => {
+  runWebglProgram({
+    canvasContainer: selector,
+    fragmentShaderURL: '/assets/shaders/metaball.glsl',
+  });
+});
+
+insideCanvas(selector => {
+  runWebglProgram({
+    canvasContainer: selector,
+    fragmentShaderURL: '/assets/shaders/helix.glsl',
+    contextAttributes: {
+      alpha: true,
+      premultipliedAlpha: false,
+    },
+  });
+});
+
+insideCanvas(selector => {
+  runWebglProgram({
+    canvasContainer: selector,
+    fragmentShaderURL: '/assets/shaders/debug.glsl',
+    onFirstRender(controls) {
+      const upd = () => {
+        controls.setUniform3fv('u_value', [
+          Math.random() * 4 - 2,
+          Math.random() * 4 - 2,
+          Math.random() * 4 - 2,
+        ]);
+      };
+      setInterval(upd, 500);
+      upd();
+    },
+  });
 });
---
+++ github-demo/live-server.js
@@ -0,0 +1,21 @@
+// @ts-check
+import { exec } from 'child_process';
+import liveServer from 'live-server';
+import path from 'path';
+
+/**
+ * @type {liveServer.LiveServerParams}
+ */
+const params = {
+  port: 3200, // Set the server port. Defaults to 8080.
+  host: '0.0.0.0', // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+  root: path.resolve(process.cwd(), '../'), // Set root directory that's being served. Defaults to cwd.
+  ignore: '', // comma-separated string for paths to ignore
+  open: false,
+  file: 'github-demo/index.html', // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+  wait: 0, // Waits for all changes, before reloading. Defaults to 0 sec.
+  logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+};
+liveServer.start(params);
+
+exec(`open http://localhost:${params.port}/github-demo/index.html`);

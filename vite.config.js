697a2b752306dec86e856ec4be6812f6943d00b4
Oleksii Shnyra <shnura94@gmail.com>
2022-05-29 13:58:11 +0300

    chore: generate d.ts typings

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
@@ -24,5 +25,6 @@ export default defineConfig({
         }
       },
     },
+    dts(),
   ],
 });
